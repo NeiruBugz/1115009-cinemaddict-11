da7e7a11f649763f1c8fd57f9b19ddad3a7bfb80
Nail Badiullin <badiullin.nail@gmail.com>
2020-05-06 22:31:52 +0400

    [module4-task1] add mocks for film card

--- src/mocks/flim-mock.js
+++ src/mocks/flim-mock.js
@@ -1,4 +1,12 @@
 import {MAX_DATE, MIN_DATE} from '../consts';
+import {
+  generateEntityMock,
+  generateRandomArray,
+  generateRandomFloat,
+  generateRandomInteger,
+  generateRandomYear
+} from '../utils/helpers';
+import {generateComment} from './comments-mock';
 
 export const MOVIE_TITLES = [
   `Made for Each Other`,
@@ -42,19 +50,36 @@ export const MOVIE_GENRES = [
   `Film-Noir`
 ];
 
-import {
-  generateEntityMock,
-  generateRandomArray,
-  generateRandomFloat,
-  generateRandomInteger,
-  generateRandomYear
-} from '../utils/helpers';
-import {generateComment} from './comments-mock';
+export const DIRECTORS = [
+  `Christopher Nolan`,
+  `Stanley Kubrick`,
+  `Andrey Tarkovsky`,
+  `Guy Ritchie`,
+  `Quentin Tarantino`
+];
+
+export const COUNTRIES = [`USA`, `France`, `Germany`, `Italy`, `Russia`];
+
+export const ACTORS = [
+  `Ryan Gosling`,
+  `Emma Stone`,
+  `Christian Bale`,
+  `Emma Watson`,
+  `Daniel Craig`,
+  `Eva Green`,
+  `Ewan McGregor`,
+  `Uma Thurman`,
+  `Samuel L. Jackson`,
+  `Tom Holland`,
+  `Scarlett Johanson`
+];
+
+export const AGE_RATING = [`6+`, `12+`, `0+`, `18+`];
 
 export const generateMovie = () => {
   return {
     title: MOVIE_TITLES[generateRandomInteger(0, 6)],
-    originalTitle: ``,
+    originalTitle: MOVIE_TITLES[generateRandomInteger(0, 6)],
     poster: MOVIE_POSTERS[generateRandomInteger(0, 6)],
     description: generateRandomArray(MOVIE_DESCRIPTION_TEMPLATES, generateRandomInteger(1, 5)),
     genre: MOVIE_GENRES[generateRandomInteger(0, 6)],
@@ -62,11 +87,11 @@ export const generateMovie = () => {
     releaseDate: generateRandomYear(MIN_DATE, MAX_DATE),
     duration: `${generateRandomInteger(1, 2)}h ${generateRandomInteger(0, 59)}m`,
     comments: generateEntityMock(generateRandomInteger(1, 4), generateComment),
-    country: ``,
-    director: ``,
+    country: COUNTRIES[generateRandomInteger(0, COUNTRIES.length)],
+    director: DIRECTORS[generateRandomInteger(0, DIRECTORS.length)],
     writers: [],
-    actors: [],
+    actors: generateRandomArray(ACTORS, generateRandomInteger(0, ACTORS.length)),
     fullDescription: ``,
-    ageRating: `18+`,
+    ageRating: AGE_RATING[generateRandomInteger(0, 3)],
   };
 };
